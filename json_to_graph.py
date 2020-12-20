b1301e305d76542ff2f5f94bc23cb139f074f001
tarou159f <r201603849je@jindai.jp>
2020-12-20 23:19:43 +0900

    Add files via upload

--- json_to_graph.py
+++ json_to_graph.py
@@ -14,12 +14,15 @@ parser.add_argument('-y','--extname', action='store', default=None, type=str, na
 parser.add_argument('-c','--color', action='store', default=None, type=str, nargs='*')
 parser.add_argument('-o','--output', default='graph.png')
 parser.add_argument('-s','--smoothed', type=bool, default=True)
+parser.add_argument('-f', '--factor', type=float, default=0.999)
+parser.add_argument('-xl', '--xlabel', type=str)
+parser.add_argument('-yl', '--ylabel', type=str)
 
 
 args = parser.parse_args()
 #args = parser.parse_args("-i openaigym.episode_batch.0.582126.stats.json openaigym.episode_batch.0.601630.stats.json -n RND PADDPG -y episode_rewards episode_rewards -c red blue".split())
 #args = parser.parse_args("-i aa bb -n cc dd -c a b".split())
-def smoothed(points, factor=0.999):
+def smoothed(points, factor=args.factor):
     smoothed_points = []
     for point in points:
         if smoothed_points:
@@ -30,6 +33,7 @@ def smoothed(points, factor=0.999):
     return smoothed_points
 
 
+# TODO: 色の自動振り分けほしい
 def main():
     fig = plt.figure(figsize=(12.8, 7.2))
     # input の数だけgraph作成
@@ -41,6 +45,8 @@ def main():
             y = smoothed(y)
         x = range(0, len(y))
         plt.plot(x, y, label=args.name[index], color=args.color[index])
+    plt.xlabel(args.xlabel, fontsize=18)
+    plt.ylabel(args.ylabel, fontsize=18)
     plt.legend(fontsize=18)
     plt.tick_params(labelsize=18)
     plt.tight_layout()
